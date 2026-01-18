869b7baf9ff0a363eaf348ae5e320008939e3917
mjnderesende <mjnderesende@gmail.com>
2026-01-17 22:35:44 -0300

    Update Exercise 01.py
    Primeira finalização, falta revisar

--- Exercise 01.py
+++ Exercise 01.py
@@ -9,8 +9,8 @@
 tamanho = float(input('Insira o tamanho da propriedade em hectares (ha): '))
 print('')
 reserva = float(input('Insira quantos hectares (ha) são preservados atualmente: '))
-reserva_porcentagem = (reserva / tamanho) * 100
-
+reserva_porcentagem = (reserva / tamanho) 
+print('')
 print('Digite a sigla da Unidade da Federação em que se encontra a propriedade.')
 print('')
 print('Acre (AC)')
@@ -43,12 +43,14 @@ print('Tocantins (TO)')
 print('')
 uf = input('Insira a sigla: ')
 if uf in ['AC', 'AP', 'AM', 'MT', 'PA', 'RO', 'RR', 'TO', 'MA']:
+    # Dentro da Amazônia Legal
+    print('')
     print('A propriedade está dentro da Amazônia Legal. Informe o em qual área ela se encontra:')
     print('')
     print('[1] - Área de Florestas')
     print('[2] - Área de Cerrado')
     print('[3] - Área de Campos Gerais')
-    print()
+    print('')
     bioma = int(input('Digite o número correspondente: '))
     if bioma == 1:
         preservacao_necessaria = 0.8
@@ -57,9 +59,19 @@ if uf in ['AC', 'AP', 'AM', 'MT', 'PA', 'RO', 'RR', 'TO', 'MA']:
     elif bioma == 3:
         preservacao_necessaria = 0.2
     else:
-        print('[ERRO] Digite um valor válido')
+        print('')
+        print('[ERRO] VALOR INVÁLIDO')
 else:
+    # Fora da Amazônia Legal
+    print('')
+    print('A propriedade se encontra fora da Amazônia Legal.')
     preservacao_necessaria = 0.2
 if preservacao_necessaria > reserva_porcentagem :
-    print('A propriedade em questão preserva mais do que a legislação (Lei 12.651/2012) exige.')
-
+    print('')
+    print(f'A propriedade em questão preserva MENOS do que a legislação (Lei 12.651/2012) exige. Um total de {(preservacao_necessaria - reserva_porcentagem)*100: .2f}% de insuficiência.')
+elif preservacao_necessaria < reserva_porcentagem :
+    print('')
+    print(f'A propriedade em questão preserva MAIS do que a legislação (Lei 12.651/2012) exige. Um total de {(reserva_porcentagem - preservacao_necessaria)*100: .2f}% de excedente.')
+else:
+    print('')
+    print('A propriedade em questão preserva EXATAMENTE o que a legislação (Lei 12.651/2012) exige.')
