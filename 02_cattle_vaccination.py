5f2898f94d1aeac53d88e7c0f1db8e4e61afe33d
mjnderesende <mjnderesende@gmail.com>
2026-01-19 20:30:15 -0300

    Update 02_cattle_vaccination.py
    First completion of the exercise

--- 02_cattle_vaccination.py
+++ 02_cattle_vaccination.py
@@ -11,23 +11,36 @@
 count_bezerro = 0
 count_novilho = 0
 count_boi = 0
-idade = 1
 
-while idade != 0 :
-    idade = int(input('Insira a idade em meses de todo animal com no mínimo 1 mês de vida: '))
-    if idade <= 12:
-        count_bezerro += 1
-    elif idade <= 24:
-        count_novilho += 1
-    elif idade >= 25:
-        count_boi += 1
-    else:
-        print('[ERRO] VALOR INVÁLIDO')
-        exit()
+print('Insira as idades em meses e digite [0] para encerrar.')
 
-if count_novilho > 0:
-    partes_da_frase.append(f"{count_novilho} novilhos")
-if count_boi > 0:
-    partes_da_frase.append(f"{count_boi} bois")
-if count_bezerro > 0:
-    partes_da_frase.append(f"{count_bezerro} bezerros")
+while True :
+    try:
+        idade = int(input('Idade (meses): '))
+        if idade == 0:
+            break
+        elif idade < 0:
+            print('[ERRO] Valor inferior a [0].')
+            continue
+        elif idade <= 12:
+            count_bezerro += 1
+        elif idade <= 24:
+            count_novilho += 1
+        else:
+            count_boi += 1
+        
+    except ValueError:
+        print('[ERRO] Digite apenas números inteiros')
+    
+
+print('A quantidade de animais contados foi:')
+print(f'Bezerros: {count_bezerro};')
+print(f'Novilhos: {count_novilho};')
+print(f'Bois: {count_boi}.')
+
+vacinas = count_bezerro + count_novilho
+total_rebanho = count_novilho + count_bezerro + count_boi
+
+print('')
+print(f'Total do rebanho: {total_rebanho};')
+print(f'Total de vacinas necessárias: {vacinas}.')
